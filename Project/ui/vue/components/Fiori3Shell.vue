7bdd96daef880e45c52c5eda85646fa1eda9f93e
Adam Yao <a.yao@sap.com>
2019-03-06 18:16:23 +0800

    binding fiori3

---
+++ Project/ui/vue/components/Fiori3Shell.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="fd-shell fd-shell--fundamentals">
+    <div class="fd-shell__header">fd-shell__header</div>
+    <div class="fd-shell__app">
+      <div class="fd-app">
+        <main class="fd-app__main">fd-app__main</main>
+      </div>
+    </div>
+    <div class="fd-shell__footer">fd-shell__footer</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fiori3Shell',
+  props: {
+    msg: String
+  }
+}
+</script>
--- Project/ui/vue/main.js
+++ Project/ui/vue/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FundamentalVue from 'fundamental-vue'
 
+Vue.use(FundamentalVue)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
