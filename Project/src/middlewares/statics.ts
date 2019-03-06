983bccfba16108160d9ffab0b27e074e93d78d35
Adam Yao <a.yao@sap.com>
2019-03-06 15:30:16 +0800

    type script && vue

--- Project/src/middlewares/statics.ts
+++ Project/src/middlewares/statics.ts
@@ -4,5 +4,8 @@ import * as express from 'express'
 
 export default (app: express.Application) => {
   app.use(compress())
-  app.use('/ui', express.static(path.resolve('./vue')))
+  app.use('/ui', express.static(path.resolve('./ui/webapp')))
+  app.use('/css', express.static(path.resolve('./ui/webapp/css')))
+  app.use('/js', express.static(path.resolve('./ui/webapp/js')))
+  app.use('/img', express.static(path.resolve('./ui/webapp/img')))
 }
