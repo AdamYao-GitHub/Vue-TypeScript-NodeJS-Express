8a89e8af01eb1c2f564e2fadda6b1f90f4dac59a
Adam Yao <Adamyao@yeah.net>
2019-07-10 17:55:02 +0800

    shell template

---
+++ client/components/shell.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  div(class="MainShell")
+    div(class="firstColumn")
+      slot(name="firstColumn")
+    div(class="secondColumn")
+      slot(name="secondColumn")
+    div(class="thirdColunm")
+      slot(name="thirdColunm")
+</template>
+<script>
+export default {
+  name: "ShellComponent",
+  methods: {
+    firstColumnFullScreen() {},
+    secondColumnFullScreen() {},
+    thirdColumnFullScreen() {}
+  },
+  data: () => ({}),
+  props: {},
+  mounted() {}
+};
+</script>
